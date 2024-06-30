512261d093f3d3e2d3d387011ac9fce67a8ed08f
yoonki1207 <yoonki1207@gmail.com>
2024-06-30 16:42:00 +0900

    init

---
+++ src/main/java/com/matzipmap/mzm_memberservice/MzmMemberserviceApplication.java
@@ -0,0 +1,13 @@
+package com.matzipmap.mzm_memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzmMemberserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzmMemberserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matzipmap/mzm_memberservice/MzmMemberserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matzipmap.mzm_memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzmMemberserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
