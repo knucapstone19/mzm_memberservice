816508319c14e9e061bd20a52cb2529cf9f71d89
yoonki1207 <yoonki1207@gmail.com>
2024-11-04 16:57:15 +0900

    Feat: EnableJpaAuditing - CreateAt 자동 설정 등

--- src/main/java/com/matzipmap/mzm_memberservice/MzmMemberserviceApplication.java
+++ src/main/java/com/matzipmap/mzm_memberservice/MzmMemberserviceApplication.java
@@ -2,8 +2,10 @@ package com.matzipmap.mzm_memberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MzmMemberserviceApplication {
 
     public static void main(String[] args) {
